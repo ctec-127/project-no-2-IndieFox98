2cc7ca1e54a1cca46f5a7d15fa8e7dbc7ebee7fa
IndieFox98 <ttashima7@gmail.com>
2019-02-11 10:04:40 -0800

    Project No. 2 Submission

--- page1.php
+++ page1.php
@@ -10,9 +10,45 @@
     <title>Project 2 - Page 1</title>
 </head>
 <body>
-
-
-
+    <?php
+        require "header.html";
+    ?>
+    <section>
+        <br>
+        <h2 class="centered"><strong>Build Your Furry Self!</strong></h2>
+        <?php
+            if (isset($_GET['form'])) {
+                echo '<p class="centered">Please fill out form below.</p><br>';
+                require "form.html";
+                echo '<br>';
+            } else {
+                echo '<p class="centered">Follow instructions below.</p><br>';
+                echo '<p>This particular generator has been designed to teach fellow PHP beginners about URL parameters.
+                These show up in the URL when submitting data using the GET method. In order to understand URL parameters,
+                we ask that you enter them in yourself to see the desired result.</p>
+                <p class="centered">Here is the syntax: ?parameter1=value1&amp;parameter2=value2...</p><br>
+                <p>The set of URL parameters goes at the <strong>very end</strong> of the URL string. For this tutorial,
+                there are <strong>seven</strong> URL parameters to focus on, several with limited values. Here is the list
+                (<strong>parameter name</strong>: values).</p>
+                <ul><li><strong>animal</strong>: "fox", "lynx", "raccoon"</li>
+                <li><strong>attitude</strong>: "caustic", "exuberant", "hotheaded"</li>
+                <li><strong>name</strong>: any value</li><li><strong>word</strong>: any value</li>
+                <li><strong>bmonth</strong> (birth month): 01-12 (double digits recommended)</li>
+                <li><strong>bdate</strong> (birth date): 01-31 (double digits recommended)</li>
+                <li><strong>byear</strong> (birth year): 1900-2019 (preferred range)</li>
+                </ul><br>
+                <p>To get the gist of it, here\'s an example. I recommend you copy and paste this. <strong>NOTE</strong>: The file
+                for this web page is "<strong>page1.php</strong>". To witness the result, change it to "<strong>page2.php</strong>".</p>
+                <p class="centered">?animal=fox&amp;attitude=caustic&amp;name=Randy&amp;word=car&amp;bmonth=10&amp;bdate=04&amp;byear=1988</p><br>
+                <p>Congratulations! You now know how to make a fursona using URL parameters!</p><br>
+                <p>Still overwhelmed? You can opt to use a form instead by clicking "GO TO FORM" below.</p><br>
+                <a href="page1.php?form">GO TO FORM</a><br>';
+            }
+        ?>
+    </section>
+    <?php
+        require "footer.html";
+    ?>
 <!-- jQuery -->
 <script src="js/jquery-3.3.1.min.js"></script>
 <!-- Bootstrap JavaScript -->
--- page2.php
+++ page2.php
@@ -10,9 +10,115 @@
     <title>Project 2 - Page 2</title>
 </head>
 <body>
+    <?php
+        require "header.html";
+    ?>
+    <section>
+        <?php
+            // check for missing url parameters
+            $required = ['animal', 'attitude', 'name', 'word', 'bmonth', 'bdate', 'byear'];
+            $errors = [];
+            for ($i=0; $i < count($required); $i++) { 
+                if (!isset($_GET[$required[$i]]) or empty($_GET[$required[$i]])) {
+                    $errors[count($errors)] = $required[$i];
+                }
+            }
 
-
-
+            // execute if none of the required url parameters are missing
+            if (count($errors) == 0) {
+                $animal = $_GET['animal'];
+                $attitude = $_GET['attitude'];
+                $name = $_GET['name'];
+                $word = $_GET['word'];
+                $bmonth = $_GET['bmonth'];
+                if ($bmonth < 1) {
+                    $bmonth = 1;
+                } else if ($bmonth > 12) {
+                    $bmonth = 12;
+                }
+                $bdate = $_GET['bdate'];
+                if ($bdate < 1) {
+                    $bdate = 1;
+                } else if ($bdate > 31) {
+                    $bdate = 31;
+                }
+                $byear = $_GET['byear'];
+                if ($byear < 1900) {
+                    $byear = 1900;
+                } else if ($byear > 2019) {
+                    $byear = 2019;
+                }
+                $dob = new DateTime($byear . '-' . $bmonth . '-' . $bdate);
+                $today = new DateTime();
+                $age = $today->diff($dob)->y;
+                echo '<br><h2 class="centered"><strong>Introducing your new fursona, ' . $name . ' the ' . $animal . '!</strong></h2><br>';
+                switch ($animal) {
+                    case 'fox':
+                        echo '<img src="img/red-fox-14939022938mj.jpg" /><br>';
+                        break;
+                    case 'lynx':
+                        echo '<img src="img/bobcat-1516716737EBL.jpg" /><br>';
+                        break;
+                    case 'raccoon':
+                        echo '<img src="img/raton-laveur-4.jpg" /><br>';
+                        break;
+                    default:
+                        echo '<p class="centered">Image of ' . $animal . ' is not available.</p><br>';
+                        break;
+                }
+                switch ($attitude) {
+                    case 'caustic':
+                        echo '<p';
+                        if (isset($_GET['comic'])) {
+                            echo ' class="comic"';
+                        }
+                        echo '>Hey. My name is ' . $name . '. The world currently wasted ' . $age . ' years of my life. I shouldn\'t
+                        really have to mention this but I am a ' . $animal . '. I hate it when people ask me if I am a ' . $animal .
+                        '. I would be like, "No, of course not! In fact, I happen to be a ' . $word . ' disguised as a ' . $animal .
+                        '." More often than I would hope, people would believe my sarcastic reply because they\'re stupid like that.</p>';
+                        break;
+                    case 'exuberant':
+                        echo '<p';
+                        if (isset($_GET['comic'])) {
+                            echo ' class="comic"';
+                        }
+                        echo '>Oh, <em>hiiii!</em> How you doin\'? I am ' . $name . ' the ' . $animal . '! ¡Yo tengo ' . $age . ' años,
+                        eyyyy! Are you having a good day? Let me tell you a little about myself. I am a ' . $animal . ' who likes ' .
+                        $word . '! I also like to play the ' . $word . ', eat ' . $word . ', speak ' . $word . ', and breathe ' . $word .
+                        '! Oops, sorry. Didn\'t mean to get too excited. Hehe.</p>';
+                        break;
+                    case 'hotheaded':
+                        echo '<p';
+                        if (isset($_GET['comic'])) {
+                            echo ' class="comic"';
+                        }
+                        echo '>Howdy! My name is ' . $name . '! Don\'t call me a ' . $animal . ' because that\'s insulting to
+                        me and my mother! Plus, I\'m ' . $age . '! What in blazes are doing here anyway?! You best get out of my way
+                        or I\'ll send the cops after you for invasion of privacy! Unless you give me ' . $word . ' this instant!
+                        Then I\'ll forgive your insolent bottom! Yes, I have to end all my sentences in exclamation marks. Because
+                        that\'s how I am, you ' . $word . '!</p>';
+                        break;
+                    default:
+                        echo '<p class="centered">The ' . $attitude . ' attitude is not available.</p>';
+                        break;
+                }
+            } else {
+                echo '<br><p class="centered">The following URL parameters are missing:</p>';
+                for ($i=0; $i < count($errors); $i++) { 
+                    echo '<p class="centered">' . $errors[$i] . '</p>';
+                }
+                echo '<br><p>GLaDOS: "You\'ve managed to corrupt the Fursona Generator with your epic lack of competence. Congratulations."</p>';
+            }
+        ?>
+        <br>
+        <a href="page1.php">START OVER</a>
+        <br>
+        <a href="page1.php?form">START OVER WITH FORM</a>
+        <br>
+    </section>
+    <?php
+        require "footer.html";
+    ?>
 <!-- jQuery -->
 <script src="js/jquery-3.3.1.min.js"></script>
 <!-- Bootstrap JavaScript -->
